04458ed1558628ccae02be754f39a63eda73b510
Violetta Shavlis <violetta.shavlis@mail.ru>
2018-05-05 00:54:34 +0300

    init Spring project

---
+++ src/main/java/com/bsuir/spp/SPPApplication.java
@@ -0,0 +1,12 @@
+package com.bsuir.spp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SPPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SPPApplication.class, args);
+	}
+}
---
+++ src/main/java/com/bsuir/spp/config/AppConfig.java
@@ -0,0 +1,31 @@
+package com.bsuir.spp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class AppConfig {
+
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties("spring.datasource")
+//    public DataSource dataSource(){
+//        return DataSourceBuilder.create().build();
+//    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedHeaders("*");
+            }
+        };
+    }
+
+}
