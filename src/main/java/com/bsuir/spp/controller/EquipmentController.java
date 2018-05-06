595a5816091f7066eb98fce35ee7ba2629cf0948
Violetta Shavlis <violetta.shavlis@mail.ru>
2018-05-07 00:31:13 +0300

    fix path

--- src/main/java/com/bsuir/spp/controller/EquipmentController.java
+++ src/main/java/com/bsuir/spp/controller/EquipmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Equipment")
+@RequestMapping("/equipment")
 public class EquipmentController {
 
     private final EquipmentService service;
