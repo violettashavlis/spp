c5287a487efddb6972273b424e9aa7b6564bfb24
Violetta Shavlis <violetta.shavlis@mail.ru>
2018-05-07 00:31:33 +0300

    Add many to many

--- src/main/java/com/bsuir/spp/model/ServiceEntity.java
+++ src/main/java/com/bsuir/spp/model/ServiceEntity.java
@@ -3,6 +3,7 @@ package com.bsuir.spp.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString @EqualsAndHashCode
@@ -23,4 +24,33 @@ public class ServiceEntity {
     private int countPeople;
     @Column(name="serv_description",length = 100)
     private String description;
+
+    @ManyToMany
+    @JoinTable(name="m2m_serv_tool",
+            joinColumns=
+            @JoinColumn(name="serv_id", referencedColumnName="serv_id"),
+            inverseJoinColumns=
+            @JoinColumn(name="tool_id", referencedColumnName="tool_id")
+    )
+    private List<Tool> tools;
+
+    @ManyToMany
+    @JoinTable(name="m2m_serv_material",
+            joinColumns=
+            @JoinColumn(name="serv_id", referencedColumnName="serv_id"),
+            inverseJoinColumns=
+            @JoinColumn(name="material_id", referencedColumnName="material_id")
+    )
+    private List<Material> materials;
+
+    @ManyToMany
+    @JoinTable(name="m2m_serv_equipment",
+            joinColumns=
+            @JoinColumn(name="serv_id", referencedColumnName="serv_id"),
+            inverseJoinColumns=
+            @JoinColumn(name="equipment_id", referencedColumnName="equipment_id")
+    )
+    private List<Equipment> equipments;
+
+
 }
